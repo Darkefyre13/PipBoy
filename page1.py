cb9180b266b934e08ee17d576c355da6e798b960
DarkeFyre13 <oliverhayden12@gmail.com>
2019-02-05 13:06:46 +1300

    Create Page1

---
+++ page1.py
@@ -0,0 +1,15 @@
+import sys
+import tkinter
+from tkinter import constants
+from tkinter import Tk
+
+
+root = Tk()
+root.title('PipBoy3k')
+root.attributes('-fullscreen', True)
+
+root.mainloop()
+
+
+sleep(30)
+exit()
