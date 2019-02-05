da5346b635b94b0e658973ec7abdf4eae2a92819
DarkeFyre13 <oliverhayden12@gmail.com>
2019-02-06 10:55:28 +1300

    Just me screwing round and learning

--- page1.py
+++ page1.py
@@ -1,15 +1,20 @@
 import sys
 import tkinter
+import time
 from tkinter import constants
-from tkinter import Tk
+from tkinter import Tk, Label
 
 
 root = Tk()
 root.title('PipBoy3k')
-root.attributes('-fullscreen', True)
+root.geometry("800x480+50+50")
+
+
+text_loading = Label(root, text='Loading, Please Wait')
+text_loading.config(font=('times', 48))
+text_loading.pack()
+time.sleep(5)
+text_loading.pack_forget()
+
 
 root.mainloop()
-
-
-sleep(30)
-exit()
