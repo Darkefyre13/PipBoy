3ba4ebe29ab9ba4b0a37425039dc6f23b3c6b4c5
DarkeFyre13 <oliverhayden12@gmail.com>
2019-02-06 11:10:22 +1300

    trying something

--- page1.py
+++ page1.py
@@ -2,10 +2,11 @@ import sys
 import tkinter
 import time
 from tkinter import constants
-from tkinter import Tk, Label
+from tkinter import Tk, Label, Message, font
 
 
 root = Tk()
+root.configure(background='black')
 root.title('PipBoy3k')
 root.geometry("800x480+50+50")
 
@@ -13,8 +14,15 @@ root.geometry("800x480+50+50")
 text_loading = Label(root, text='Loading, Please Wait')
 text_loading.config(font=('times', 48))
 text_loading.pack()
-time.sleep(5)
 text_loading.pack_forget()
+
+class Page1(tk.frame)
+
+
+
+
+
+
 
 
 root.mainloop()
