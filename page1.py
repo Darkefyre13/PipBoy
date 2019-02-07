e5a6dd726d02a7107d221a715600098890a23c9c
DarkeFyre13 <oliverhayden12@gmail.com>
2019-02-07 13:58:31 +1300

    I think i broke it

--- page1.py
+++ page1.py
@@ -1,28 +1,46 @@
 import sys
-import tkinter
+#import tkinter
+import tkinter as tk
 import time
 from tkinter import constants
-from tkinter import Tk, Label, Message, font
+from tkinter import Tk, Label, Message, font, Menu, Button, Toplevel
+from tkinter.ttk import Progressbar
 
 
 root = Tk()
 root.configure(background='black')
 root.title('PipBoy3k')
-root.geometry("800x480+50+50")
+#root.geometry("800x480+50+50")
+root.attributes('-fullscreen', True)
+
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Quit", command=root.destroy)
+
+exit_button = Button(root, command=root.destroy)
+exit_button.pack(side="bottom")
+
+class Loading(tk.Frame):
+    """docstring for loading."""
+    def __init__(self, arg):
+        super(Loading, self).__init__()
+        self.arg = arg
+        progress=Progressbar(tk,length=100,mode='determinate')
+
+def bar():
+    import time
+    progress['value']=20
+    tk.update_idletasks()
+    time.sleep(1)
+    progress['value']=50
+    tk.update_idletasks()
+    time.sleep(1)
+    progress['value']=80
+    tk.update_idletasks()
+    time.sleep(1)
+    progress['value']=100
+    Loading.pack_forget()
 
 
-text_loading = Label(root, text='Loading, Please Wait')
-text_loading.config(font=('times', 48))
-text_loading.pack()
-text_loading.pack_forget()
-
-class Page1(tk.frame)
-
-
-
-
-
-
-
-
+Loading.pack()
 root.mainloop()
